4afe1ad639fce00f463f8a931f77afffc27ea481
Neeraj <neerajbhatt264@gmail.com>
2023-12-19 21:44:01 +0530

    LinkedList class and some methods

--- LinkedList/LLEndInsert.cpp
+++ LinkedList/LLEndInsert.cpp
@@ -1,51 +1,126 @@
 #include <iostream>
 using namespace std;
-class Node{
+class node{
     public:
     int val;
-    Node* next;
-    Node(){}
-    Node(int v){
+    node* next;
+    node(){}
+    node(int v){
         val = v;
         this->next = NULL;
     }
 };
-void printll(Node* head)
-{
-    if(head == NULL)
+
+class linkedList{
+    node* head = NULL;
+    node* tail = NULL;
+    public:
+    //default constructor
+    linkedList(){}
+    // takes an array and it's size to get a linked list
+    linkedList(int* arr, int size);
+    void insert(int val);
+    void deletion(void);            
+    void printll(void);
+};
+
+linkedList :: linkedList(int* arr, int size){
+    if(arr != NULL){
+        head = new node(arr[0]);
+        tail = head;
+        int arrsize = size/sizeof(int);
+        int num = 1;
+        while(num < arrsize){
+            tail->next = new node(arr[num]);
+            tail = tail->next;
+            num++;
+        }
+    }
+    else{
+        cout << "Array is empty" << endl;
+    }
+}
+
+// inserts new elements in the end
+void linkedList :: insert(int val){
+    if(head == NULL){
+        head = new node(val);
+        tail = head;
+    }
+    else{
+        tail->next = new node(val);
+        tail = tail->next;
+    }
+    cout << "Sucess " << endl;
+}
+
+//deletes the last inserted element LIFO
+void linkedList :: deletion(void){
+    if(head == NULL){
         cout << "There is no Linked List" << endl;
+    }
+    else{
+        if(tail == head){
+            tail = NULL;
+            delete(head);
+            head = NULL;
+            cout << "Deleted successfully" << endl;
+            cout << "Linked List Deleted successfully" << endl;
+        }
+        else{
+            node* temp = head;
+            while(temp->next != tail){
+                temp = temp->next;
+            }
+            delete(tail);
+            tail = temp;
+            tail->next = NULL;
+            cout << "Deleted successfully" << endl;
+        }
+    }
+
+}
+
+// print elements of the linked list
+void linkedList :: printll(void){
+    node* temp = head;
+    if(temp == NULL)
+        cout << "There is no Linked List to Print" << endl;
     else{
         cout << "Linked List : ";
-        while(head != NULL){
-            cout << head->val << " ";
-            head = head->next;
+        while(temp != NULL){
+            cout << temp->val << " ";
+            temp = temp->next;
         }
         cout << endl;
     }
+    temp = NULL;
 }
 
 int main()
 {
     char ch = 'Y';
     int value;
-    Node* head = NULL;
-    Node* ptr = NULL;
+    linkedList ll1;
     while(ch == 'Y' || ch == 'y')
     {
         cout << "Enter a value to add in a Linked List" << endl;
         cin >>  value;
-        if(head == NULL)
-            head = new Node(value);
-        else{
-            ptr = head;
-            while(ptr->next != NULL)
-                ptr = ptr->next;
-            ptr->next = new Node(value);
-        }
-        printll(head);
+        ll1.insert(value);
         cout << endl << "Y/y to continue" << endl;
         cin >> ch;
     }
-
+    ll1.printll();
+    // int array[6] = {1,2,3,4,5,6};
+    // linkedList ll2(array, sizeof(array));       // pass an array and size of it
+    // ll2.printll();
+    // ll2.deletion();
+    // ll2.deletion();
+    // ll2.deletion();
+    // ll2.deletion();
+    // ll2.deletion();
+    // ll2.deletion();     // here all the elements are deleted and linked list gets empty
+    // ll2.deletion();     // there is no element to delete
+    // ll2.printll();
     return 0;
 }
--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -12,10 +12,59 @@ class node{
     }
 };
 
-void printll(node* head){
+class linkedList{
+    node* head;
+    node* tail;
+    public:
+    linkedList(){       //default constructor
+        head = NULL;
+        tail = NULL;
+    }  
+    linkedList(int* arr, int arraysize);    //constructor for an array
+    ~linkedList();  //destructor
+    void insert_frnt(int val);
+    void insert_rear(int val);
+    void del_frnt();
+    void del_rear();
+    void reverse_linkedlist();
+    void print_linkedlist(); 
+
+};
+
+//destructor
+linkedList :: ~linkedList(){
+    node* des = NULL;
+    tail = NULL;
+    while(head != NULL){
+        des = head;
+        head = head->next;
+        delete(des);
+    }
+}
+
+//array into linked list
+linkedList :: linkedList(int* arr, int arraysize){
+    if(arr == NULL)
+        return;
+
+    int array_elements = arraysize/sizeof(int);
+    int count = 1;
+    head = new node(arr[0]);
+    tail = head;
+    if(array_elements>1){
+        while(count<array_elements){
+            tail->next = new node(arr[count]);
+            tail = tail->next;
+            count++;
+        }
+    }
+}
+
+//print elements of linked list
+void linkedList :: print_linkedlist(){
     node* ptr = head;
     if(head == NULL)
-        cout << "There in no Linked List" << endl;
+        cout << "There in no Linked List to print" << endl;
     else{
         cout << "Linked List : ";
         while(ptr != NULL)
@@ -26,92 +75,110 @@ void printll(node* head){
         cout << endl;
     }
 }
-node* del(node* head, int value){
-    node* ptr1 = head;
-    if(head == NULL)
-        cout << "There is nothing to Delete" << endl;
-    else{
-        while(ptr1 != NULL)
-        {        
-            if(head->val == value){
-                head = head->next;
-                delete(ptr1);
-                ptr1 = head;
-            }
-            else{
-                node* p = head;
-                while(ptr1->val != value && ptr1->next != NULL){
-                    p = ptr1;
-                    ptr1 = ptr1->next;
-                }
-                if(ptr1->next == NULL && ptr1->val != value)
-                    ptr1 = NULL;
-                else{
-                    p->next = ptr1->next;
-                    delete(ptr1);
-                    ptr1 = p->next;
-                }
-            }
-        }
+
+void linkedList :: reverse_linkedlist(){
+    if(head == NULL || head->next == NULL){
+        return;
     }
-    return head;
+    node* prevnode = NULL;
+    node* current = head;
+    node* nextnode = NULL;
+    while(current != NULL){
+        nextnode = current->next;   //stores the next pointer
+        current->next = prevnode;   //reverse node
+        
+        prevnode = current;     //one step
+        current = nextnode;     //increases
+    }
+    //update head and tail pointers
+    tail = head;
+    head = prevnode;
+    prevnode = NULL;
+}
+
+// deletion of an element from head
+// head is moved accordingly
+void linkedList :: del_frnt(){
+    node* temp = NULL;
+    if(head == NULL)
+        cout << "Linked List is empty" << endl;
+    else{
+        temp = head;
+        if(head == tail)
+            tail = NULL;
+        head = head->next;
+        cout << "Deleted Successfully : " << temp->val << endl;
+        delete(temp);
+    }
+}
+
+// deletion of an element from rear
+// tail is moved accordingly
+void linkedList :: del_rear(){
+    node* temp = NULL;
+    if(head == NULL)
+        cout << "Linked List is empty" << endl;
+    else{
+        if(head == tail){
+            head = NULL;
+            delete(tail);
+        }
+        else{
+            temp = head;
+            while(temp->next != tail){
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            cout << "Deleted Successfully : " << tail->val << endl;
+            delete(tail);
+            tail = temp;
+            }
+    }
+}
+
+//inserts an element in the beginning of a linked list
+void linkedList :: insert_frnt(int val){
+    if(head == NULL){
+        tail = new node(val);
+        head = tail;
+    }
+    else{
+        node* temp = new node(val);
+        temp->next = head;
+        head = temp;
+    }
+}
+
+//inserts an element at the end of a linked list
+void linkedList :: insert_rear(int val){
+    if(head == NULL){
+        head = new node(val);
+        tail = head;
+    }
+    else{
+        tail->next = new node(val);
+        tail = tail->next;
+    }
 }
 
 int main()
 {
-    int value;
-    char ch = 'y';
-    node* head = NULL;
-    node* ptr = NULL;
-    node* ptr1 = NULL;
-    while(ch == 'Y' || ch == 'y')
-    {
-        cout << "Enter a value in Linked List" << endl;
-        cin >> value;
-        if(head == NULL)
-            head = new node(value);
-        else{
-            if(value > head->val)
-            {
-                ptr = head;
-                while(value > ptr->val && ptr->next != NULL)
-                {
-                    ptr1 = ptr;
-                    ptr = ptr->next;
-                }
-                if(value > ptr->val)  //for adding new node at the end
-                    ptr->next = new node(value);
-                else{                 //for adding new node anywhere between Linked list
-                    ptr1->next = new node(value);
-                    (ptr1->next)->next = ptr;
-                }
+    linkedList ll1;
+    ll1.insert_frnt(5);
+    ll1.insert_frnt(6);
+    ll1.insert_rear(8);
+    ll1.insert_frnt(9);
+    ll1.print_linkedlist();
+    // ll1.del_frnt();
+    // ll1.print_linkedlist();
+    // ll1.del_rear();
+    // ll1.print_linkedlist();
+    ll1.reverse_linkedlist();
+    ll1.print_linkedlist();
 
-            }
-            //when value is less than head->value
-            //node is added at beginning and head is now pointing to new starting node
-            else{
-                ptr = new node(value);
-                ptr->next = head;
-                head = ptr;
-            }
-        }
-        printll(head);
-        cout << endl << "Enter 'y/Y to continue or anything else to exit" << endl;
-        cin >> ch;
-        
-    }
-    cout << "For Deletion, Enter y : " << endl;
-    cin >> ch;
-    while(ch == 'y' || ch == 'Y'){
-        printll(head);
-        cout << endl << "Enter a value to delete : " << endl;
-        cin >> value;
-        head = del(head, value);
-        cout << "After Deletion" << endl;
-        printll(head);
-        cout << "Enter y to continue : ";
-        cin >> ch;
-    }
+    int arr[6] = {1,2,3,4,5,6};
+    linkedList ll2(arr, sizeof(arr));
+    ll2.print_linkedlist();
 
     return 0;
 }
